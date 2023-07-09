f5caea7b92357b187f89e0d2f5e25fd5b9b8a472
dev-adriano-lira <igorrs.rib@gmail.com>
2023-07-09 01:37:07 -0300

    feat: add getters to paciente

--- src/main.js
+++ src/main.js
@@ -3,7 +3,14 @@ import PromptSync from 'prompt-sync';
 const prompt = PromptSync({ sigint: true });
 
 function mainMenu(){
-    const option = prompt("Menu principal\n1-Cadastro de pacientes\n2-Agenda\n3-Fim");
+    const option = prompt("Menu principal\n1-Cadastro de pacientes\n2-Agenda\n3-Fim\n");
+
+    switch (option){
+        default:
+            console.log("Opção inválida, escolha novamente.");
+            mainMenu();
+            break;
+    }
 }
 
 mainMenu()
--- src/paciente.js
+++ src/paciente.js
@@ -2,11 +2,14 @@ class Paciente {
     #cpf;
     #nome;
     #dataNascimento;
+    #consultas;
+
   
     constructor(cpf, nome, dataNascimento) {
       this.#cpf = cpf;
       this.#nome = nome;
       this.#dataNascimento = dataNascimento;
+      this.#consultas = [];
     }
   
     get cpf() {
@@ -20,5 +23,10 @@ class Paciente {
     get dataNascimento() {
       return this.#dataNascimento;
     }
+
+    get consultas() {
+        return this.#consultas;
+      }
   }
   
+export default Paciente;
